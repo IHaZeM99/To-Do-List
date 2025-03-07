b4a824777eabbd3dcedbc972edfcb1ef7adcb680
IHaZeM99 <hazem.a9021@gmail.com>
2025-03-07 17:31:00 +0200

    improved js

--- script.js
+++ script.js
@@ -1,31 +1,22 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
-let tasksByDate = JSON.parse(localStorage.getItem("tasksByDate")) || {};
 
 function addTask(){
-    let inputValue = inputBox.value.trim();
-    let dateInput = document.getElementById('myDateInput').value;
-    if(inputValue === ''){
-        // alert("You must write something");
-        Swal.fire({
+    if(inputBox.value === ''){
+        //alert("You must write something");
+	Swal.fire({
             icon: "error",
             title: "Oops...",
             text: "You must write something!",
           });
-        return;
-    }if (!tasksByDate[dateInput]) {
-        tasksByDate[dateInput] = []; 
     }else{
-        // let li =document.createElement("li");
-        // li.innerHTML = inputBox.value;
-        // listContainer.appendChild(li);
+        let li =document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
         inputBox.value = '';  
-        // let span = document.createElement("span");
-        // span.innerHTML = '\u00d7';
-        // li.appendChild(span);
-
-        tasksByDate[dateInput].push(inputValue);
-        displayData(document.getElementById('myDateInput').value);
+        let span = document.createElement("span");
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
     }
     saveData();
 }
@@ -41,39 +32,9 @@ listContainer.addEventListener("click",function(e){
 },false);
 
 function saveData(){
-    localStorage.setItem("tasksByDate", JSON.stringify(tasksByDate));
     localStorage.setItem("data",listContainer.innerHTML);
 }
 function loadData(){
-    tasksByDate.innerHTML = localStorage.getItem("tasksByDate", JSON.stringify(tasksByDate))
     listContainer.innerHTML = localStorage.getItem("data");
-    let selectedDate = document.getElementById('myDateInput').value;
 }
-
-function getDate(){
-    const today = new Date();
-    const formattedDate = today.toLocaleDateString('en-CA');
-    document.getElementById('myDateInput').value = formattedDate;    
-    return formattedDate;
-}
-
-function displayData(date){
-    listContainer.innerHTML = '';
-    tasksByDate[date].array.forEach(element => {
-        let li =document.createElement("li");
-        li.innerHTML = element
-        listContainer.appendChild(li);
-        // inputBox.value = '';  
-        let span = document.createElement("span");
-        span.innerHTML = '\u00d7';
-        li.appendChild(span);
-    });
-}
-document.getElementById("myDateInput").addEventListener("change",function(){
-    let selectedDate = document.getElementById("myDateInput").value;
-    displayData(selectedDate);
-})
-
-displayData(getDate);
-getDate();
 loadData();
